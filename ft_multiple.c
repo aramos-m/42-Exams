836e319c41fc8a133b7d0a7d02fbe81c5b707cc1
aramos-m <anaana609@gmail.com>
2023-10-25 19:57:26 +0200

    PiscinaAgosto2023

---
+++ ft_multiple.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_multiple(char *str)
+{
+    int i;
+
+    i=0;
+    while (str[i]!='\0')
+    {
+        if((i%3==0) && (i%5==0))
+        {
+            write(1,"5",1);
+            i++;
+        }
+        if(i%5==0)
+        {
+            write(1,"3",1);
+            i++;
+        }
+        if(i%3==0)
+        {
+            write(1,"5",1);
+            i++;
+        }
+        else
+        {
+            write(1,&str[i],1);
+            i++;
+        }
+    }
+	return ;
+}
+/*int main (void)
+{
+	char saludo [] = "Hola me llamo Ana";
+	
+	ft_multiple(saludo);
+	printf("\nResultado esperado: 5ol5 35 l53m5 a5a");
+	return (0);
+}*/
